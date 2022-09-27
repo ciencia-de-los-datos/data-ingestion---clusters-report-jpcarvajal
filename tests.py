0147d5cf30acadd3052db3e257341db2782a7574
Juan Pablo <jpcarvajalga@unal.edu.co>
2022-09-27 03:34:56 -0500

    a

--- tests.py
+++ tests.py
@@ -11,6 +11,7 @@ import pregunta
 def test_01():
     assert pregunta.ingest_data().cluster.to_list() == list(range(1, 14))
 
+
 def test_02():
     assert pregunta.ingest_data().cantidad_de_palabras_clave.to_list() == [
         105,
